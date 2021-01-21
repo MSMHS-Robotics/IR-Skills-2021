d5f4a1718fdbc9256568f78761e32e3b7be7a003
nickynoodle16 <nicholas.howard@msmrockets.org>
2021-01-21 17:14:31 -0600

    Started the shooter code, mostly copied

--- code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,19 +1,53 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
+// IGNORE THE WARNINGS THEY DONT FEEL LOVE
 package frc.robot.subsystems;
 
+// Motors
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+// Encoders, PID's, and other things 
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.ControlType;
+
+// Shuffleboard
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+// Other
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj.Timer;
+import java.util.Map; 
+
 
 public class ShooterSubsystem extends SubsystemBase {
-    /** Creates a new ExampleSubsystem. */
+  // Motors
+  private CANSparkMax shooterMotor;
+  private CANSparkMax shooterMotor2;
+
+  // Control
+  private CANPIDController shooterPID;
+  private CANEncoder encoder;
+
+  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOUtput;
+  private double RPMSetpoint;
+  private Boolean shootingFlag;
+
+  private double pastTime = 0;
+  private double pastVelocity = 0;
+  private double shotAcceleration = 0; 
+
     public ShooterSubsystem() {
     }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    
   }
 
   @Override
