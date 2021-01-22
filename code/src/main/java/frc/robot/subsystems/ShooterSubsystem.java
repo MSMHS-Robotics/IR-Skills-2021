df3d05b10ff23f62069a8f1c09be406516f46834
nickynoodle16 <nicholas.howard@msmrockets.org>
2021-01-22 16:49:35 -0600

    set up motors

--- code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -45,7 +45,50 @@ public class ShooterSubsystem extends SubsystemBase {
   private double pastVelocity = 0;
   private double shotAcceleration = 0; 
 
-    public ShooterSubsystem() {
+  // Shuffleboard things commented out for my own sanity, uncomment this once constants are there
+  /*private ShuffleboardTab tab1 = Shuffleboard.getTab("Shooter");
+  private NetworkTableEntry ShooterkP = tab1.addPersistent("ShooterkP", constants.ShooterkPdivide).getEntry();
+  private NetworkTableEntry kPdivide = tab1.addPersistent("kPdivide", Constants.ShooterkPdivide).getEntry();
+  private NetworkTableEntry ShooterkI = tab1.addPersistent("ShooterkI", Constants.ShooterkI).getEntry();
+  private NetworkTableEntry kIdivide = tab1.addPersistent("kIdivide", Constants.ShooterkIdivide).getEntry();
+  private NetworkTableEntry ShooterkD = tab1.addPersistent("ShooterkD", Constants.ShooterkD).getEntry();
+  private NetworkTableEntry kDdivide = tab1.addPersistent("kDdivide", Constants.ShooterkDdivide).getEntry();
+  private NetworkTableEntry ShooterkIz = tab1.addPersistent("ShooterkIz", Constants.ShooterkIz).getEntry();
+  private NetworkTableEntry ShooterkFF = tab1.addPersistent("ShooterkFF", Constants.ShooterkFF).getEntry();
+  private NetworkTableEntry kFFdivide = tab1.addPersistent("kFFdivide", Constants.ShooterkFFdivide).getEntry();
+  private NetworkTableEntry ShooterkMaxOutput = tab1.addPersistent("ShooterkMaxOutput", Constants.ShooterkMaxOutput).getEntry();
+  private NetworkTableEntry ShooterkMinOutput = tab1.addPersistent("ShooterkMinOutput", Constants.ShooterkMinOutput).getEntry();
+  private NetworkTableEntry RPMTolerance = tab1.addPersistent("RPMTolerance", Constants.RPMTolerance).getEntry();
+  private NetworkTableEntry ShooterRPM = tab1.addPersistent("ShooterRPM", 0).getEntry();
+  private NetworkTableEntry neededRPM = tab1.addPersistent("Vision Needed RPM", 0).getEntry();
+  private NetworkTableEntry ShotAcceleration = tab1.addPersistent("Shot Acceleration", 0).getEntry();
+  private NetworkTableEntry AccelerationTolerance = tab1.addPersistent("Acceleration Tolerance",Constants.accelerationTolerance).getEntry();
+  private NetworkTableEntry isShooterGood = tab1.addPersistent("is Shooter Good", false).withWidget("Boolean Box").withProperties(Map.of("colorWhenTrue", "green", "colorWhenFalse", "red")).getEntry();
+  private NetworkTableEntry isShooting = tab1.addPersistent("Shooter is Shooting", false).withWidget("Boolean Box").withProperties(Map.of("colorWhenTrue", "green", "colorWhenFalse", "red")).getEntry();
+  
+  private ShuffleboardTab tab2 = Shuffleboard.getTab("Shooter Presets");
+  private NetworkTableEntry TrenchRPM = tab2.addPersistent("TrenchRPM", Constants.TrenchRPM).getEntry();
+  private NetworkTableEntry TenFootRPM = tab2.addPersistent("TenFootRPM", Constants.TenFootRPM).getEntry();
+  private NetworkTableEntry LayupRPM = tab2.addPersistent("LayupRPM", Constants.LayupRPM).getEntry();
+
+  private ShuffleboardTab toggleTab = Shuffleboard.getTab("Toggle Tab");
+	private NetworkTableEntry toggleDiag = toggleTab.add("Comp Mode?", false).withWidget(BuiltInWidgets.kToggleButton).getEntry();
+  */
+
+    public ShooterSubsystem(int shooter1Port, int shooter2Port) {
+      shootingFlag = false;
+      shooterMotor = new CANSparkMax(7, MotorType.kBrushless);
+      shooterMotor2 = new CANSparkMax(8, MotorType.kBrushless);
+
+      if (shooterMotor != null) {
+        shooterMotor.setInverted(true); // Inverted because it is opposite the other motor
+        shooterPID = shooterMotor.getPIDController();
+        encoder = shooterMotor.getEncoder();
+      }
+
+      if (shooterMotor2 != null){
+        shooterMotor2.follow(shooterMotor, true);
+      }
     }
 
   @Override
