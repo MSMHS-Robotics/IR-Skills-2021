c3d4f0afd585a4e59eb8b94a777eb87f74ccdc9b
justin.zondor@msmrockets.org <nicholas.howard@msmrockets.org>
2021-02-01 18:08:21 -0600

    Added funcvtions for zoom and things

--- code/src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ code/src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -1,9 +1,49 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class LimelightSubsystem extends SubsystemBase {
+    public boolean isZoomed = false;
+    public boolean isAligned = false; 
+    public NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+
+    // Turns the LED's off and on
+    private void ledsOff() {
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(0);
+    }
+
+    private void ledsOn() {
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(1);
+    }
+    
+    // Zooms and unzooms
+    private void zoom() {
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(1);
+        isZoomed = true; 
+    }
+
+    private void unZoom() {
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(0);
+        isZoomed = false;
+    }
+
+    // Tells whether we are zoomed/aligned 
+    private boolean checkAlign() {
+      return isAligned; 
+    }
+
+    private boolean checkZoom() {
+        return isZoomed;
+    }
+
+    public void toggleAlign() {
+        
+    }
   public LimelightSubsystem() {
+
       
   }
 
